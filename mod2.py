8f07aeccd173c28cddb7362787c60020d1aafdb2
Anu Benoy <43821109+anubenoy@users.noreply.github.com>
2020-10-02 16:27:13 +0530

    mod2p1

---
+++ mod2.py
@@ -0,0 +1,34 @@
+# def oe(n):                            #1
+#     if(n%2==0):print("even")
+#     else: print("odd")
+# oe(int(input("enter a no:-")))
+
+# def n_str(s):             #3
+#     if(s[:2].lower() == 'is'):return s
+#     return 'Is'+ s
+# print(n_str(input("enter a str:-")))
+
+# l1=['aa','bb','cc']
+# l2=['aa','cc']
+# common= set(l1).intersection(l2)
+# if len(common) !=0:
+#     print("true")
+
+# import functools
+# n=list('123')
+# print(functools.reduce(lambda x,y:int(x)+int(y),n))
+
+# print(sum([int(x)for x in list("123")]))
+
+# def n_copies(str,n):              #4
+#     return str*n
+# print(n_copies(input("enter a str"),int(input("enter no of copies-"))))
+
+
+# def sum3(l):               #2
+#     sum=l.sum()
+#     if 
+# lis=[]
+# for x in range(3):             
+#     lis.append(int(input("enter a no:--")))
+# sum3(lis)
